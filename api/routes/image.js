27fe5cb7004ee3605fc70fbcb9da6ac1d2200899
abhijeetiyer2211 <abhijeetiyer@whitehastjr.com>
2020-08-23 18:41:43 +0530

    commit

--- api/routes/image.js
+++ api/routes/image.js
@@ -5,29 +5,21 @@ const isImageURL = require('is-image-url');
 router.get('/',(req,res,next) => {
     let imageUrl = req.query.imageUrl;
     if(isImageURL(imageUrl)){
-
         const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
-        let imageStyle = req.query.imageStyle;
-        let operationType = req.query.type;
-        deepai.setApiKey('4d24e2da-8259-4497-8db3-ca2ebf4b1d04');
-        
-        if(operationType === 'colorizer'){
-            
-            let resultImage = '';
-            async function process() {
-                var resp = await deepai.callStandardApi("colorizer", {
-                    image: imageUrl,
-                });    
-
-                resultImage = resp.output_url;
-                res.status(200).json({
-                    message:`${operationType} operation success`,
-                    image_url:imageUrl,
-                    output_url:resultImage 
-                });
-            }
-            process();
+        deepai.setApiKey('4d24e2da-8259-4497-8db3-ca2ebf4b1d04');   
+        let resultImage = '';
+        async function process() {
+        var resp = await deepai.callStandardApi("colorizer", {
+            image: imageUrl,
+        });    
+        resultImage = resp.output_url;
+        res.status(200).json({
+            message:`${operationType} operation success`,
+            image_url:imageUrl,
+            output_url:resultImage 
+            });
         }
+        process();
     }
     else{
         res.status(200).json({
